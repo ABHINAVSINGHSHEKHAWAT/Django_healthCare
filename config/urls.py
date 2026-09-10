8f0a250bea6c27979fbc37abf2d4adbeb4354c0c
Abhianv Shekahwat <e22cseu1606@bennett.edu.in>
2026-09-11 04:28:47 +0530

    added doctor CRUD API

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/patients/", include("patients.urls")),
+    path("api/doctors/", include("doctors.urls")),
 
 ]
