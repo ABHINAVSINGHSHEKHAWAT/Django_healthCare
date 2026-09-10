85830edd80b2ecc9595d4f24d1bf98fed1288b13
Abhianv Shekahwat <e22cseu1606@bennett.edu.in>
2026-09-11 03:57:29 +0530

    setup Django healthcare backend with authentication and patient API

---
+++ accounts/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    class Meta:
+        model = User
+        fields = ["name", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            name=validated_data["name"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        return user
---
+++ patients/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+
+
+class Patient(models.Model):
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="patients",
+    )
+    name = models.CharField(max_length=150)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
